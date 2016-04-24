5b978694f3a6e4d65d42c0c03e5bc4a3641f46e8
matteomts <hmateus1997@gmail.com>
2016-04-23 21:28:13 -0300

    Update JogoUFPA.java

--- src/jogo/ufpa/JogoUFPA.java
+++ src/jogo/ufpa/JogoUFPA.java
@@ -1,12 +1,21 @@
-
 package jogo.ufpa;
 
+import javax.swing.JFrame;
 
-public class JogoUFPA {
+public class JogoUFPA extends JFrame{
 
+    public JogoUFPA(){
+        add(new Background());
+        setTitle("Jogo UFPA");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1000,768);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+}
     
     public static void main(String[] args) {
-        
+        new JogoUFPA();
     }
     
 }
